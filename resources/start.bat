55d0b3647613ea474b8c4f40fc31a7b278e1d255
ZorinArch <62427713+ZorinArch@users.noreply.github.com>
2022-11-13 20:18:11 +0200

    fix group and set new password

--- resources/start.bat
+++ resources/start.bat
@@ -3,8 +3,8 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2022 By ZorinRDP" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user zorin Action4ever!! /add >nul
-net localgroup zorins zorin /add >nul
+net user zorin Zorin@RDP! /add >nul
+net localgroup administrators zorin /add >nul
 net user zorin /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -16,6 +16,6 @@ echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: zorin
-echo Password: Action4ever!!
+echo Password: Zorin@RDP!
 echo You can login now!
 ping -n 10 127.0.0.1 >nul
