74334d814624e4670c8b98a25b9df6b26931f68a
ZorinArch <62427713+ZorinArch@users.noreply.github.com>
2022-11-13 20:30:06 +0200

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,21 +1,21 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2022 By ZorinRDP" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2022 By Zorin" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user zorin Zorin@RDP! /add >nul
-net localgroup administrators zorin /add >nul
-net user zorin /active:yes >nul
+net user Zorin Z37Yh%ajq^njA$JU /add >nul
+net localgroup administrators Zorin /add >nul
+net user Zorin /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant zorin:F >nul
-ICACLS C:\Windows\installer /grant zorin:F >nul
+ICACLS C:\Windows\Temp /grant Zorin:F >nul
+ICACLS C:\Windows\installer /grant Zorin:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: zorin
-echo Password: Zorin@RDP!
+echo Username: Zorin
+echo Password: Z37Yh%ajq^njA$JU
 echo You can login now!
 ping -n 10 127.0.0.1 >nul
